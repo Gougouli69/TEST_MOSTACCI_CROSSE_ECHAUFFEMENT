ef0c1d26a82d6c793d26ba5aa99ca47672112548
Gregory <gregory.mostacci@ynov.com>
2023-12-11 11:57:47 +0100

    nolog: init the main branch

--- README.md
+++ README.md
@@ -1,7 +1,1 @@
-# Echauffement METHODO Tests
-
-Install the dependencies with `composer install`
-
-Run the game with `php console.php`
-
-Run the tests with `php run tests`
+# METHODO Tests
--- console.php
+++ console.php
@@ -1,30 +1,3 @@
 <?php
 
 require_once "vendor/autoload.php";
-
-use App\Classes\TimePalindrome;
-
-$dateNow = new DateTime(date("H:i:s"));
-$timePalindrome = new TimePalindrome();
-
-// Say hi-word in console at the begging 
-$timePalindrome->sayInConsole(
-    $timePalindrome->getHiMessageDependingOnDayMoment($dateNow)
-);
-
-// Mirror the input user as long as the input is not empty
-while ($userInput = readline("Saisissez votre texte (vide pour quitter): ")) {
-    $timePalindrome->sayInConsole(
-        strrev($userInput)
-    );
-
-    // Comment if it's a Palindrome
-    $isAPalindrome = $timePalindrome->textIsAPalindrome($userInput);
-    if($isAPalindrome)
-        $timePalindrome->isAPalindromeSuccessText($isAPalindrome);
-} 
-
-// Say bye-word at the end
-$timePalindrome->sayInConsole(
-    $timePalindrome->getByeMessageDependingOnDayMoment($dateNow)
-);
