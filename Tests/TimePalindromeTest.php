345a49e9d489b9483d76bebbc4d3a96e5f28f164
Gregory <gregory.mostacci@ynov.com>
2023-12-11 11:10:50 +0100

    feat: add tests to match palindome

---
+++ Tests/TimePalindromeTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Tests;
+
+use App\Classes\TimePalindrome;
+use PHPUnit\Framework\TestCase;
+
+final class TimePalindromeTest extends TestCase {
+
+    public function testIsAPalindrome()
+    {
+        $timePalindrome = new TimePalindrome();
+
+        $this->assertEquals(true, $timePalindrome->textIsAPalindrome('KayAk'));
+        $this->assertEquals(false, $timePalindrome->textIsAPalindrome('Chocolat'));
+        $this->assertEquals(false, $timePalindrome->textIsAPalindrome('Bonjour'));
+        $this->assertNotEquals(false, $timePalindrome->textIsAPalindrome('Mon nOm'));
+    }
+}
