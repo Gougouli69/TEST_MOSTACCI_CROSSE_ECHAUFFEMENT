f6dfbc7bd852e4436f562d1c623909b308aa6fb4
Gregory <gregory.mostacci@ynov.com>
2024-01-21 14:47:27 +0100

    nolog: missing file

--- src/Domain/Classes/Moment/EveningMoment.php
+++ src/Domain/Classes/Moment/EveningMoment.php
@@ -6,6 +6,6 @@ namespace Domain\Classes\Moment;
 class EveningMoment implements MomentInterface {
 
     const HIWORD = "BONSOIR";
-    const BYEWORD = "BONSOIR";
+    const BYEWORD = "BONSOIR_BYE";
 
 }
